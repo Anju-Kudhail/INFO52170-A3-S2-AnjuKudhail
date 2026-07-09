d1e41d258d20d24c2b7e9408e4e7dfe1a73c2e55
Anju-Kudhail <anjunayyar.pm@gmail.com>
2026-07-08 22:30:08 -0400

    Introduce bug for CI demo - Anju Kudhail

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # calculator.py - Created by Anju Kudhail
 def add(a, b):
-    return a-b # Bug: subtracting instead of adding
+    return a -b     # Bug: subtracting instead of adding
 
 
 def subtract(a, b):
